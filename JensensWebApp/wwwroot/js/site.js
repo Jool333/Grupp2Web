32fd2fee6f4a7da95ec422f1781e57bc67350aa6
Arshia257 <hosseiniarshia5@gmail.com>
2023-06-07 22:09:09 +0200

    Nya och äldre artikel kort

--- JensensWebApp/wwwroot/js/site.js
+++ JensensWebApp/wwwroot/js/site.js
@@ -15,3 +15,72 @@
         }
     }
 }
+
+
+/* Show more for other articles */
+function loadMore() {
+    var LoadMoreBTN = document.querySelector("#show-more");
+    var CurrentItem = 8;
+    var isShowMore = true;
+    LoadMoreBTN.onclick = () => {
+        var boxes = [...document.querySelectorAll('.New-card')];
+        if (isShowMore) {
+            for (var i = CurrentItem; i < CurrentItem + 8; i++) {
+                if (i < boxes.length) {
+                    boxes[i].style.display = 'inline';
+                }
+            }
+            CurrentItem += 8;
+            if (CurrentItem >= boxes.length) {
+                LoadMoreBTN.innerHTML = 'Show less';
+                isShowMore = false;
+            }
+        } else {
+            CurrentItem -= 8;
+            for (var i = CurrentItem; i < CurrentItem + 8; i++) {
+                if (i < boxes.length) {
+                    boxes[i].style.display = 'none';
+                }
+            }
+            if (CurrentItem <= 8) {
+                LoadMoreBTN.innerHTML = 'Show more';
+                isShowMore = true;
+            }
+        }
+    }
+}
+
+function loadMoreOld() {
+    var LoadMoreBTN = document.querySelector("#show-more-old");
+    var CurrentItem = 8;
+    var isShowMore = true;
+    LoadMoreBTN.onclick = () => {
+        var boxes = [...document.querySelectorAll('.Other-card')];
+        if (isShowMore) {
+            for (var i = CurrentItem; i < CurrentItem + 8; i++) {
+                if (i < boxes.length) {
+                    boxes[i].style.display = 'flex';
+                }
+            }
+            CurrentItem += 8;
+            if (CurrentItem >= boxes.length) {
+                LoadMoreBTN.innerHTML = 'Show less';
+                isShowMore = false;
+            }
+        } else {
+            CurrentItem -= 8;
+            for (var i = CurrentItem; i < CurrentItem + 8; i++) {
+                if (i < boxes.length) {
+                    boxes[i].style.display = 'none';
+                }
+            }
+            if (CurrentItem <= 8) {
+                LoadMoreBTN.innerHTML = 'Show more';
+                isShowMore = true;
+            }
+        }
+    }
+}
+loadMore();
+
+loadMoreOld();
