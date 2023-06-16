9b4d151638c17c53a7a68101c69f9bf1049914ea
Jool333 <jool333@live.se>
2023-06-16 12:04:26 +0200

    kopplat väder api, alla behöver dock en egen nyckel då de är begränsade i anv, ska funka men mina försök är slut atm

--- JensensWebApp/wwwroot/js/weather.js
+++ JensensWebApp/wwwroot/js/weather.js
@@ -1,51 +1,62 @@
 const weatherCodeMap = new Map();
 weatherCodeMap.set(0, 'Unknown');
-weatherCodeMap.set(1000, 'Clear');
-weatherCodeMap.set(1001, 'Cloudy');
-weatherCodeMap.set(1100, 'Mostly Clear');
-weatherCodeMap.set(1101, 'Partly Cloudy');
-weatherCodeMap.set(1102, 'Mostly Cloudy');
-weatherCodeMap.set(2000, 'Fog');
-weatherCodeMap.set(2100, 'Light Fog');
-weatherCodeMap.set(3000, 'Light Wind');
-weatherCodeMap.set(3001, 'Wind');
-weatherCodeMap.set(3002, 'Strong Wind');
-weatherCodeMap.set(4000, 'Drizzle');
-weatherCodeMap.set(4001, 'Rain');
-weatherCodeMap.set(4200, 'Light Rain');
-weatherCodeMap.set(4201, 'Heavy Rain');
-weatherCodeMap.set(5000, 'Snow');
-weatherCodeMap.set(5001, 'Flurries');
-weatherCodeMap.set(5100, 'Light Snow');
-weatherCodeMap.set(5101, 'Heavy Snow');
-weatherCodeMap.set(6000, 'Freezing Drizzle');
-weatherCodeMap.set(6001, 'Freezing Rain');
-weatherCodeMap.set(6200, 'Light Freezing Rain');
-weatherCodeMap.set(6201, 'Heavy Freezing Rain');
-weatherCodeMap.set(7000, 'Ice Pellets');
-weatherCodeMap.set(7101, 'Heavy Ice Pellets');
-weatherCodeMap.set(7102, 'Light Ice Pellets');
-weatherCodeMap.set(8000, 'Thunderstorm');
+weatherCodeMap.set(1000, 'clear');
+weatherCodeMap.set(1001, 'cloudy');
+weatherCodeMap.set(1100, 'mostly_clear_day');
+weatherCodeMap.set(1101, 'partly_cloudy');
+weatherCodeMap.set(1102, 'mostly_cloudy');
+weatherCodeMap.set(2000, 'fog');
+weatherCodeMap.set(2100, 'light_fog');
+weatherCodeMap.set(3000, 'light_wind');
+weatherCodeMap.set(3001, 'wind');
+weatherCodeMap.set(3002, 'strong_wind');
+weatherCodeMap.set(4000, 'drizzle');
+weatherCodeMap.set(4001, 'rain');
+weatherCodeMap.set(4200, 'light_rain');
+weatherCodeMap.set(4201, 'heavy_rain');
+weatherCodeMap.set(5000, 'snow');
+weatherCodeMap.set(5001, 'flurries');
+weatherCodeMap.set(5100, 'light_snow');
+weatherCodeMap.set(5101, 'heavy_snow');
+weatherCodeMap.set(6000, 'freezing_drizzle');
+weatherCodeMap.set(6001, 'freezing_rain');
+weatherCodeMap.set(6200, 'light_freezing_rain');
+weatherCodeMap.set(6201, 'heavy_freezing_rain');
+weatherCodeMap.set(7000, 'ice_pellets');
+weatherCodeMap.set(7101, 'heavy_ice_pellets');
+weatherCodeMap.set(7102, 'light_ice_pellets');
+weatherCodeMap.set(8000, 'thunderstorm');
 
 const dayCodeMap = new Map();
 // weatherModel.js
 const WeatherModel = {
     fetchWeatherData: async (lat, long) => {
-        const apiKey = 'QwLHgOoFjKpuhJgwJjXDAzSB1jdtbNrd';
+        const apiKey = 'personlig api nyckel'; //alla borde göra en egen för bästa resultat
         const mainUrl = "https://api.tomorrow.io/v4/timelines?location=";
         const optionsUrl = `${lat},${long}&fields=temperature,weatherCode&timesteps=1d&units=metric&apikey=${apiKey}`;
         const apiUrl = mainUrl + optionsUrl;
-        //const apiUrl = "https://api.tomorrow.io/v4/timelines?location=55.17,13.19&fields=temperature,weatherCode&timesteps=1d&units=metric&apikey=QwLHgOoFjKpuhJgwJjXDAzSB1jdtbNrd";
         const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-        fetch(apiUrl, options)
+        const weatherdata = fetch(apiUrl, options)
             .then(response => response.json())
             .then(data => {
-                const mainData = data.timelines[0].intervals;
+                console.log(data);
+                const intervals = data.data.timelines[0].intervals.map(interval => ({
+                    startTime: interval.startTime,
+                    temperature: interval.values.temperature,
+                    weatherCode: interval.values.weatherCode
+                }));
+
+                return intervals;
             })
+            /*.then(intervalsArray => {
+                console.log(intervalsArray);
+                // Use the intervalsArray here or pass it to another function
+            })*/
             .catch(error => {
                 console.error('Error fetching JSON:', error);
-            });        
+            });
+        return weatherdata;
     }
 }
 // weatherView
@@ -54,8 +65,9 @@ const WeatherView = {
         const temperatureElement = document.getElementById('temperature');
         const conditionElement = document.getElementById('condition');
         // Update DOM elements with weather information
-        temperatureElement.textContent = `${weatherInfo[0].temperature}°C`;
-        conditionElement.textContent = weatherInfo.weatherCode;
+        temperatureElement.textContent = `${Math.round(weatherInfo[0].temperature)}\u00B0C`;
+        const imgSrc = weatherCodeMap.get(weatherInfo[0].weatherCode);
+        conditionElement.src = `images/weather/${imgSrc}.svg`;
     },
 };
 
@@ -69,7 +81,7 @@ weatherButton.addEventListener('click', () => {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
                 const temperatureElement = document.getElementById('temperature');
-                temperatureElement.textContent = `lat: ${latitude} long: ${longitude}`;
+                //temperatureElement.textContent = `lat: ${latitude} long: ${longitude}`;
                 
                 // Call the model to fetch weather data with coordinates
                 const weatherData = await WeatherModel.fetchWeatherData(latitude, longitude);
